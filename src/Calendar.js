2966f3570e59b49f54feb38e6cd1e5c62bc98a20
Georgios Anastasiou <gsiou@gunet.gr>
2019-07-19 12:00:02 +0300

    Calendar component

---
+++ src/Calendar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const goToPage  = () => {
+    window.location.href = '#start'
+}
+
+export default () => (
+    <div className="App">
+      <p>Calendar page</p>      
+    </div>
+);
