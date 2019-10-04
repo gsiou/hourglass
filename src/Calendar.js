9441188ab19fcf813db43f10e2a673f37b7842dd
Georgios Anastasiou <gsiou@gunet.gr>
2019-10-04 16:38:41 +0300

    Store input and calculate weeks alive

--- src/Calendar.js
+++ src/Calendar.js
@@ -4,8 +4,16 @@ const goToPage  = () => {
     window.location.href = '#start'
 }
 
-export default () => (
+export default () => {
+  const today = new Date().getTime() / 1000;
+  const bday = parseInt(localStorage.getItem('bday'));
+  const lifespan = parseInt(localStorage.getItem('lifespan'));
+
+  const weeksAlive = (today - bday) / 60 / 60 / 24 / 7;
+  return (
     <div className="App">
-      <p>Calendar page</p>      
+      <p>Calendar page</p>
+      <p>Weeks alive: {weeksAlive}</p>
     </div>
-);
+  );
+}
--- src/Landing.js
+++ src/Landing.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "@reach/router"
 
 const goToPage  = () => {
     window.location.href = '#start'
 }
 
-export default () => (
+export default () => {
+  const [bday, setBday] = useState(new Date(0));
+  const [lifespan, setLifespan] = useState(80);
+
+  const store = () => {
+    localStorage.setItem('bday', JSON.stringify(Math.floor(new Date(bday).getTime()/1000)));
+    localStorage.setItem('lifespan', JSON.stringify(lifespan * 365 * 24 * 60 * 60));
+  }
+
+  return (
     <div className="App">
       <header className="App-header">
         <h1>H o u r g l a s s</h1>
@@ -13,14 +22,15 @@ export default () => (
       </header>
       <div className="Frontpage" id="start">
           <p>
-            I was born on <input type="date"/>
+            I was born on <input onChange={(e) => setBday(e.target.value)} value={bday} type="date"/>
           </p>
           <p>
-            I believe I'll die at the age of <input type="number" max={120}/>
+            I believe I'll die at the age of <input onChange={(e) => setLifespan(e.target.value)} value={lifespan} type="number" max={120}/>
           </p>
           <div className="Action">
-            <Link to="calendar" className="Next">Next <span className="fas fa-long-arrow-alt-right"/></Link>
+            <Link onClick={store} to="calendar" className="Next">Next <span className="fas fa-long-arrow-alt-right"/></Link>
           </div>
         </div>
     </div>
-);
+  );
+}
